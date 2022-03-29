2be878a18f9b91919623b20c2dfdf21e42111c5a
shaaran-habteq <shaaran.habteq@gmail.com>
2022-03-29 04:33:57 -0700

    Display all questions

--- app/User.php
+++ app/User.php
@@ -29,8 +29,5 @@ class User extends Authenticatable
     public function questions(){
         return $this->hasMany(Question::class);
     }
-    public function setTitleAttribute($value){
-        $this->attributes['title']=$value;
-        $this->attributes['slug']=str_slug($value);
-    }
+    
 }
